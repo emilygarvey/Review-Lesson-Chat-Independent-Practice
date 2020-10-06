ffaca0f5eaa4d6988c87e8e588d5552545350ed2
Emily Garvey <41160828+emilygarvey@users.noreply.github.com>
2020-10-06 12:15:04 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//DON'T CHANGE THIS CODE
+
+let realYourName, realFriendName;
+
+try {
+    realYourName = eval('yourName');
+    realFriendName = eval('friendsName');
+} catch (_e) {}
+
+updateNames();
+
+function updateNames(){
+    $(".your-name").text(realYourName);
+    $(".friends-name").text(realFriendName);
+}
